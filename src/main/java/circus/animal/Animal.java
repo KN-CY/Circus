167d48a6ce305ee00525dd7113cc8b53af95279d
KN-CY <kaemoncyng@gmail.com>
2023-02-14 16:44:31 +0800

    Update code according to lecture instructions

--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -2,8 +2,19 @@ package circus.animal;
 
 import circus.Asset;
 
+import java.util.Comparator;
+
 public abstract class Animal implements Asset {
 
     public String name;
     public abstract String speak();
+
+    // create a comparator object
+    public static Comparator<Animal> AnimalNameComprator = new Comparator<Animal>() {
+        @Override
+        public int compare(Animal animal1, Animal animal2) {
+
+            return animal1.name.compareToIgnoreCase(animal2.name);
+        }
+    }; // semicolon cos this is technically a single line of code
 }
